ec24f889b77482b40a2cb351498caf3c6ede6bdc
Arthur <amarthur@usp.br>
2022-07-07 10:49:03 -0300

    Split processing into sequential and parallel

--- src/process_log.py
+++ src/process_log.py
@@ -1,5 +1,6 @@
 import re
 import csv
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -11,9 +12,13 @@ class Mandelbroter():
         self.file_path = file_path
         self.file_name = file_name
 
-    def process_data(self):
+    def process_data(self, is_sequential):
         self.create_csv_file()
-        self.plot_csv()
+
+        if is_sequential:
+            self.plot_sequential()
+        else:
+            self.plot_parallel()
 
     def create_csv_file(self):
         log_file = self.file_path + '.log'
@@ -46,7 +51,26 @@ class Mandelbroter():
 
                     writer.writerow(row); row = []
 
-    def plot_csv(self):
+    def plot_sequential(self):
+        df = pd.read_csv(self.file_path + '.csv')
+        y_coord = self.header[2]
+
+        # Create a plot comparing different image sizes
+        image_sizes = df.groupby('Image Size')
+
+        for img_size, _ in image_sizes:
+            group = image_sizes.get_group(img_size).convert_dtypes()
+            y = group[y_coord]
+            label = f'{img_size} px'
+            plt.plot(['Without Alloc','With Alloc'], y, label=label, marker='o')
+
+        # Labels
+        self.put_essential_labels(ylabel=y_coord)
+
+        # Save plot and clear
+        self.save_plt()
+
+    def plot_parallel(self):
         df = pd.read_csv(self.file_path + '.csv')
         x_coord, y_coord = self.header[1], self.header[2]
 
@@ -58,23 +82,30 @@ class Mandelbroter():
             x = group[x_coord]
             y = group[y_coord]
             label = f'{img_size} px'
-            plt.plot(x, y, label=label, marker='o')
+            plt.plot(x, np.log2(y), label=label, marker='o')
 
-        # Put labels
-        plt.xlabel(x_coord)
-        plt.ylabel(y_coord)
-        plt.title(f'{self.exe_type} {self.file_name}')
-        plt.legend(loc='upper right', bbox_to_anchor=(1.25, 1))
+        # Labels
+        plt.xticks([2**i for i in range(6)])
+        self.put_essential_labels(xlabel=x_coord, ylabel=f'log2({y_coord})')
 
         # Save plot and clear
+        self.save_plt()
+
+    def put_essential_labels(self, xlabel="", ylabel=""):
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(f'{self.exe_type} {self.file_name}')
+        plt.legend(loc='upper right', bbox_to_anchor=(1.25, 1), title='Image Size')
+        plt.grid()
+
+    def save_plt(self):
         plot_path = self.file_path + '.png'
         plt.savefig(plot_path, bbox_inches='tight')
         plt.clf()
 
-
 def main():
     # All possible files
-    results_dir = 'results/'
+    results_dir = 'res/'
     dir_names = ['mandelbrot_seq/', 'mandelbrot_pth/', 'mandelbrot_omp/']
     file_names = ['full', 'elephant', 'seahorse', 'triple_spiral']
 
@@ -84,15 +115,17 @@ def main():
     formated_fnames = ['Full', 'Elephant', 'Seahorse', 'Triple Spiral']
 
     # Permutate and process
-    for i, dir_name in enumerate(dir_names):
-        for file_name in file_names:
-            header[1] = 'Threads' if dir_name != 'mandelbrot_seq/' else 'Alloc Memory'
-            exe_type = exe_types[i]
-            formated_fname = formated_fnames[i]
+    for d, dir_name in enumerate(dir_names):
+        for f, file_name in enumerate(file_names):
+            is_sequential = (dir_name == 'mandelbrot_seq/')
+
+            exe_type = exe_types[d]
+            formated_fname = formated_fnames[f]
             file_path = results_dir + dir_name + file_name
+            header[1] = 'Alloc Memory' if is_sequential else 'Threads'
 
             mandel = Mandelbroter(header, exe_type, file_path, formated_fname)
-            mandel.process_data()
+            mandel.process_data(is_sequential)
 
 if __name__ == '__main__':
     main()
